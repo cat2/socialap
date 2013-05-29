2c404060db0d3e9ab5785fe06f013cad621a3479
cat2 <sbezhinets@gmail.com>
2013-05-28 18:48:27 -0700

    message

--- model/auth/auth_lib.php
+++ model/auth/auth_lib.php
@@ -2,29 +2,44 @@
 
 function processAuth( $user, $pass) {
 
-	$userList = array();
-	$userList["sergey"] = "test1234";
-	$userList["admin"] = "admin1234";
-	$userList["user"] = "user1234";
-
 
 
 	$auth = array();
 	$auth["return"] = false;
 	$auth["message"] = "";
 
+
+	$sql = "SELECT *
+	        FROM auth_user
+	        WHERE username = '$user'";
+
+
+	$res = mysql_query( $sql );
+
+	$row = mysql_fetch_array( $res );
+
+
+
+
+
+
+
   // Check if user exists
-	$userArray = array_keys( $userList );
-	if ( in_array( $user, $userArray ) ) {
+	if ( $row )  {
 
 		// Check if password is valid
-		if ( $pass == $userList[$user] ) {
+		if ( $row["password"] ==  md5($pass) ) {
 			$auth["return"] = true;
 		}
+
+
+
 		else {
 			$auth["message"] = "Password is not correct!";
-		}
-	}
+
+	         }
+
+	    }
 	else {
 		$auth["message"] = "User does not exist!";
 	}
